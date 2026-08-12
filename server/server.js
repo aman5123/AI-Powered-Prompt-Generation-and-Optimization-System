b40e662173e6e1c868695be5d64d94116058bf0b
Aman <amanvrma089@gmail.com>
2026-08-12 09:51:06 +0530

    fixing server.js

--- server/server.js
+++ server/server.js
@@ -9,9 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173'
-}));
+app.use(cors());
 app.use(express.json());
 
 // Routes
