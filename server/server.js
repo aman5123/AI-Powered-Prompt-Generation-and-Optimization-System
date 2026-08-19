9cca742a352c1a65a07454cc30f8fd6b4347712e
Aman <amanvrma089@gmail.com>
2026-08-19 19:03:10 +0530

    error fix 2

--- server/server.js
+++ server/server.js
@@ -60,5 +60,5 @@ app.use((err, req, res, next) => {
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
-  console.log(`   Allowed origins: ${allowedOrigins.join(', ')}`);
+  console.log(`   Allowed origins: ${EXPLICIT_ORIGINS.join(', ')}`);
 });
